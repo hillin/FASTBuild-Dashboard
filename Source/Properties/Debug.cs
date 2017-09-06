aac7078f5682c0bbee0d85ba7e0dad5ed1c71675
hillin <hillinsilence@gmail.com>
2017-09-06 13:20:02 +0800

    move debug directives to Debug.cs

---
+++ Source/Properties/Debug.cs
@@ -0,0 +1,10 @@
+﻿// Uncomment this line if you want to debug a test log file. The test log file
+// should be located at Test/FastBuildLog.log relative to the executable
+//#define DEBUG_TEST_LOG
+
+// By default, in DEBUG config the single instance check will be skipped.
+// Uncomment this line to debug single instance.
+//#define DEBUG_SINGLE_INSTANCE
+
+// Uncomment this line to launch a debugger when starting shadow process
+//#define DEBUG_SHADOW_PROCESS
